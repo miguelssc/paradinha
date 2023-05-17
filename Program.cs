c9edc380a19c9e996d4a7043abee641c499c70aa
miguel <miguelpositivo02@gmail.com>
2023-05-17 15:50:18 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("digite o valor do produto");
+decimal valordoproduto=Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite o valor do pago");
+decimal valorpago=Convert.ToDecimal(Console.ReadLine());
+decimal troco=(valordoproduto - valorpago);
+Console.WriteLine(troco);
+if(valordoproduto>valorpago){
+    Console.WriteLine("esta faltando dinheiro");
+}
+else{
+    Console.WriteLine("ate a proxima compra");
+}
